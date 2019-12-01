795e16041b220167db5d5153e2d2a13e1f02e062
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2019-12-01 15:47:48 +0000

    Add project files.

---
+++ youme-translation-website/Controllers/FormSurfaceController.cs
@@ -0,0 +1,22 @@
+﻿using youme_website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace youme_website.Controllers
+{
+    public class FormSurfaceController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult Submit(FormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ youme-translation-website/Models/FormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace youme_website.Models
+{
+    public class FormViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
